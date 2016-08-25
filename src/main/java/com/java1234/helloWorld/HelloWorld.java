00473ba4f16cf8f11145aaa51098ca9b48eef86d
yanam <yanam@huadong.net>
2016-08-25 16:59:47 +0800

    8-25-5

--- src/main/java/com/java1234/helloWorld/HelloWorld.java
+++ src/main/java/com/java1234/helloWorld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.java1234.helloWorld;
 public class HelloWorld {
 
 	public String sayHello(){
-		return "Hello World,2016-8-25";
+		return "Hello World,2016-8-25-5";
 	}
 	
 	public static void main(String[] args) {
